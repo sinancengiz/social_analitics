dc66aaebea66589de6495f5c5214d48acd23d3f3
Sinan Cengiz <sinancengiz_84@hotmail.com>
2018-04-07 09:57:30 -0700

    datafamres created for each channel

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "eTjwMO6gxenXZqPkRaDgNDoPH"
+consumer_secret = "3EbHaER0yBLLHyIVgqHeoDw8g8D67EqbCXIp5PHOOBARNPYNPM"
+access_token = "337851157-XisprZ52E8zznIl8J9VvxxfJrrx9iArPCXDvJel8"
+access_token_secret = "S4z4DCDunOXytXGNy6FCkDSPWdnPGSFRfgW10RELHf3Cd"
--- config2.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "6kFjjU8i6CPC9Zni4ZvMdaBMU"
-consumer_secret = "o2MEOJ3Eezvnkdp6qWRnnrN3ZQMZl759xujoIBkZmBKOL5MJGd"
-access_token = "337851157-N69svE8geZC47m0nnZCMZFiZVCxIMXXRAqYV2jeO"
-access_token_secret = "jTQVHo0eAgjBeo3kApJm8zFrCDd0KQdHOXxVLjtxldZ9u"
